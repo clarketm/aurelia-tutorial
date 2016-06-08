09037bbda395360de3fa62dde5b0f999440a4609
Travis Clarke <travis.m.clarke@gmail.com>
2016-06-08 18:42:11 -0400

    html updates

--- src/home.js
+++ src/home.js
@@ -3,7 +3,7 @@ export class Home {
   constructor() {
     this.height = `${100 - 25}px`;
     this.tableClasses = "table table-bordered table-condensed table-striped table-hover";
-    this.message = "Welcome to the world of Aurelia!";
+    this.message = "Hello Aurelia!";
     this.image = "http://aurelia.io/images/main-logo.svg";
     this.name = "";
     this.answerVisible = false;
